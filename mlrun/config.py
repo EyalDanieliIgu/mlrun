86f6fcdec09ca45c4847ff0513091c3b2da534b7
Eyal-Danieli <eyald@iguazio.com>
2022-11-08 14:38:39 +0200

    add test

--- mlrun/config.py
+++ mlrun/config.py
@@ -347,8 +347,8 @@ default_config = {
         "batch_processing_function_branch": "master",
         "parquet_batching_max_events": 10000,
         # See mlrun.api.schemas.ModelEndpointStoreType for available options
-        "store_type": "sql",
-        "connection_string": "mysql+pymysql://root:pass@192.168.223.211:3306/mlrun",
+        "store_type": "kv",
+        "connection_string": "",
     },
     "secret_stores": {
         "vault": {
