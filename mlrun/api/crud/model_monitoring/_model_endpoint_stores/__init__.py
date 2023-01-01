ed71820a315a2ca5b0a8d473234db07d7f158fe3
Eyal-Danieli <eyald@iguazio.com>
2023-01-01 17:39:25 +0200

    change to kafka and sql

--- mlrun/api/crud/model_monitoring/_model_endpoint_stores/__init__.py
+++ mlrun/api/crud/model_monitoring/_model_endpoint_stores/__init__.py
@@ -107,7 +107,6 @@ def get_model_endpoint_target(
     return model_endpoint_store_type.to_endpoint_target(project, access_key)
 
 def increase_counter():
-    prometheus_client.Counter('EYAL_TEST', 'Description of counter')
     COUNTER_VALUE = prometheus_client.Counter('EYAL_TEST', 'Description of counter')
     print('[EYAL]: COUNTER_VALUE in prom created')
     COUNTER_VALUE.inc(1.5)
