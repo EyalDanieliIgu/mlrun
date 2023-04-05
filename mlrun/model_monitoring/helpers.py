ed13d605d3f3defeed0dd2e7fa77946c04824eef
Eyal-Danieli <eyald@iguazio.com>
2023-04-05 13:27:23 +0300

    fix according to saar review + validate project name in file target

--- mlrun/model_monitoring/helpers.py
+++ mlrun/model_monitoring/helpers.py
@@ -191,7 +191,9 @@ def _apply_stream_trigger(
             function.add_v3io_stream_trigger(
                 stream_path=stream_path, name="monitoring_stream_trigger"
             )
-    function.apply(mlrun.auto_mount(pvc_name="export-minio-0", volume_mount_path='/home/jovyan/data'))
+    print('[EYAL]: apply mount')
+    function.apply(mlrun.auto_mount(pvc_name="export-minio-3", volume_mount_path='/mnt/'))
+    print('[EYAL]: mounted')
     # auto_mount
     # Add the default HTTP source
     http_source = mlrun.datastore.sources.HttpSource()
