f80a24adec8251635575bf14f43e1f0cc352ea64
Eyal-Danieli <eyald@iguazio.com>
2023-07-25 11:42:45 +0300

    add new func

--- mlrun/config.py
+++ mlrun/config.py
@@ -418,7 +418,7 @@ default_config = {
         "batch_processing_function_branch": "master",
         "parquet_batching_max_events": 10000,
         # See mlrun.model_monitoring.stores.ModelEndpointStoreType for available options
-        "store_type": "sql",
+        "store_type": "v3io-nosql",
         "endpoint_store_connection": "",
     },
     "secret_stores": {
