d2dc228d48bf7d8470cdecd4247b6c1da1591258
Eyal-Danieli <eyald@iguazio.com>
2023-07-26 17:51:46 +0300

    change secret_provider signature to public

--- mlrun/db/httpdb.py
+++ mlrun/db/httpdb.py
@@ -2839,8 +2839,7 @@ class HTTPRunDB(RunDBInterface):
         params = {"default_batch_image": default_batch_image,
                   "with_schedule": with_schedule,
                   "trigger_job": trigger_job,
-                  "model_endpoints_ids": model_endpoints_ids or [],
-                  "batch_intervals_dict": batch_intervals_dict or {}}
+                  "model_endpoints_ids": model_endpoints_ids or [],}
         path = f"projects/{project}/jobs/batch-monitoring"
         # print('[EYAL]: now in deploy monitoring batch job client side, tracking: ', tracking_policy)
         print('[EYAL]: now in deploy monitoring batch job client side, path: ', path)
