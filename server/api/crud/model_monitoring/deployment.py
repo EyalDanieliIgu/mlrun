2e4ed29e06018bfb02446e833aadb60c5e229b21
Eyal-Danieli <eyald@iguazio.com>
2023-11-14 14:23:48 +0200

    lint

--- server/api/crud/model_monitoring/deployment.py
+++ server/api/crud/model_monitoring/deployment.py
@@ -629,7 +629,7 @@ class MonitoringDeployment:
         :return: function runtime object with access key and access to system files.
         """
 
-        if function_name in mm_constants.MonitoringFunctionNames.all():
+        if function_name in mm_constants.MonitoringFunctionNames.list():
             # Set model monitoring access key for managing permissions
             function.set_env_from_secret(
                 mlrun.common.schemas.model_monitoring.ProjectSecretKeys.ACCESS_KEY,
