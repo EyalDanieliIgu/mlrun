b44974e8348723bca5420e290c6913d4f46d0001
Eyal-Danieli <eyald@iguazio.com>
2024-03-13 10:08:11 +0200

    remove unused import

--- mlrun/model_monitoring/stores/application_result/application_result_store.py
+++ mlrun/model_monitoring/stores/application_result/application_result_store.py
@@ -19,127 +19,24 @@ from abc import ABC, abstractmethod
 
 class ApplicationResult(ABC):
     """
-    An abstract class to handle the model endpoint in the DB target.
+    An abstract class to handle the applications result data.
     """
 
     def __init__(self, project: str):
         """
-        Initialize a new model endpoint target.
+        Initialize a new application result target.
 
         :param project:             The name of the project.
         """
         self.project = project
 
     @abstractmethod
-    def write_application_result(self, event):
+    def write_application_result(self, event: typing.Dict[str, typing.Any]):
         """
-        Create a new endpoint record in the DB table.
+        Write a new application result record to the DB table.
 
-        :param endpoint: model endpoint dictionary that will be written into the DB.
+        :param event: Event dictionary that represents application result. In general, the dictionary structure
+                      expected to be corresponded to the .
         """
         pass
 
-    # @abstractmethod
-    # def update_model_endpoint(
-    #     self, endpoint_id: str, attributes: dict[str, typing.Any]
-    # ):
-    #     """
-    #     Update a model endpoint record with a given attributes.
-    #
-    #     :param endpoint_id: The unique id of the model endpoint.
-    #     :param attributes: Dictionary of attributes that will be used for update the model endpoint. Note that the keys
-    #                        of the attributes dictionary should exist in the DB table.
-    #
-    #     """
-    #     pass
-    #
-    # @abstractmethod
-    # def delete_model_endpoint(self, endpoint_id: str):
-    #     """
-    #     Deletes the record of a given model endpoint id.
-    #
-    #     :param endpoint_id: The unique id of the model endpoint.
-    #     """
-    #     pass
-    #
-    # @abstractmethod
-    # def delete_model_endpoints_resources(self, endpoints: list[dict[str, typing.Any]]):
-    #     """
-    #     Delete all model endpoints resources.
-    #
-    #     :param endpoints: A list of model endpoints flattened dictionaries.
-    #
-    #     """
-    #     pass
-
-    # @abstractmethod
-    # def get_model_endpoint(
-    #     self,
-    #     endpoint_id: str,
-    # ) -> dict[str, typing.Any]:
-    #     """
-    #     Get a single model endpoint record.
-    #
-    #     :param endpoint_id: The unique id of the model endpoint.
-    #
-    #     :return: A model endpoint record as a dictionary.
-    #     """
-    #     pass
-    #
-    # @abstractmethod
-    # def list_model_endpoints(
-    #     self,
-    #     model: str = None,
-    #     function: str = None,
-    #     labels: list[str] = None,
-    #     top_level: bool = None,
-    #     uids: list = None,
-    # ) -> list[dict[str, typing.Any]]:
-    #     """
-    #     Returns a list of model endpoint dictionaries, supports filtering by model, function, labels or top level.
-    #     By default, when no filters are applied, all available model endpoints for the given project will
-    #     be listed.
-    #
-    #     :param model:           The name of the model to filter by.
-    #     :param function:        The name of the function to filter by.
-    #     :param labels:          A list of labels to filter by. Label filters work by either filtering a specific value
-    #                             of a label (i.e. list("key=value")) or by looking for the existence of a given
-    #                             key (i.e. "key").
-    #     :param top_level:       If True will return only routers and endpoint that are NOT children of any router.
-    #     :param uids:             List of model endpoint unique ids to include in the result.
-    #
-    #     :return: A list of model endpoint dictionaries.
-    #     """
-    #     pass
-    #
-    # @abstractmethod
-    # def get_endpoint_real_time_metrics(
-    #     self,
-    #     endpoint_id: str,
-    #     metrics: list[str],
-    #     start: str = "now-1h",
-    #     end: str = "now",
-    #     access_key: str = None,
-    # ) -> dict[str, list[tuple[str, float]]]:
-    #     """
-    #     Getting metrics from the time series DB. There are pre-defined metrics for model endpoints such as
-    #     `predictions_per_second` and `latency_avg_5m` but also custom metrics defined by the user.
-    #
-    #     :param endpoint_id:      The unique id of the model endpoint.
-    #     :param metrics:          A list of real-time metrics to return for the model endpoint.
-    #     :param start:            The start time of the metrics. Can be represented by a string containing an RFC 3339
-    #                              time, a Unix timestamp in milliseconds, a relative time (`'now'` or
-    #                              `'now-[0-9]+[mhd]'`, where `m` = minutes, `h` = hours, and `'d'` = days), or 0 for the
-    #                              earliest time.
-    #     :param end:              The end time of the metrics. Can be represented by a string containing an RFC 3339
-    #                              time, a Unix timestamp in milliseconds, a relative time (`'now'` or
-    #                              `'now-[0-9]+[mhd]'`, where `m` = minutes, `h` = hours, and `'d'` = days), or 0 for the
-    #                              earliest time.
-    #     :param access_key:       V3IO access key that will be used for generating Frames client object. If not
-    #                              provided, the access key will be retrieved from the environment variables.
-    #
-    #     :return: A dictionary of metrics in which the key is a metric name and the value is a list of tuples that
-    #              includes timestamps and the values.
-    #     """
-    #
-    #     pass
