9f23e7452436e84469c89c919e49f578283508f4
Eyal-Danieli <eyald@iguazio.com>
2024-05-27 15:28:15 +0300

    update test function

--- mlrun/model_monitoring/db/tsdb/tdengine/tdengine_connector.py
+++ mlrun/model_monitoring/db/tsdb/tdengine/tdengine_connector.py
@@ -17,7 +17,7 @@ from datetime import datetime
 from io import StringIO
 import pandas as pd
 import taosws
-
+from mlrun.common.schemas.model_monitoring.model_endpoints import _compose_full_name
 import mlrun.common.schemas.model_monitoring as mm_schemas
 import mlrun.model_monitoring.db.tsdb.tdengine.schemas as tdengine_schemas
 import mlrun.model_monitoring.db.tsdb.tdengine.stream_graph_steps
@@ -239,6 +239,138 @@ class TDEngineConnector(TSDBConnector):
 
         return pd.DataFrame(query_result, columns=columns)
 
+    @staticmethod
+    def df_to_metrics_values(
+            *,
+            df: pd.DataFrame,
+            metrics: list[mm_schemas.ModelEndpointMonitoringMetric],
+            project: str,
+    ) -> list[
+        typing.Union[
+            mm_schemas.ModelEndpointMonitoringMetricValues,
+            mm_schemas.ModelEndpointMonitoringMetricNoData,
+        ]
+    ]:
+        """
+        Parse a time-indexed data-frame of metrics from the TSDB into a list of
+        metrics values per distinct results.
+        When a metric is not found in the data-frame, it is represented in no-data object.
+        """
+        metrics_without_data = {metric.full_name: metric for metric in metrics}
+
+        metrics_values: list[
+            typing.Union[
+                mm_schemas.ModelEndpointMonitoringMetricValues,
+                mm_schemas.ModelEndpointMonitoringMetricNoData,
+            ]
+        ] = []
+        if not df.empty:
+            grouped = df.groupby(
+                [
+                    mm_schemas.WriterEvent.APPLICATION_NAME,
+                    mm_schemas.MetricData.METRIC_NAME,
+                ],
+                observed=False,
+            )
+        else:
+            logger.debug("No metrics", missing_metrics=metrics_without_data.keys())
+            grouped = []
+        for (app_name, name), sub_df in grouped:
+            full_name = _compose_full_name(
+                project=project,
+                app=app_name,
+                name=name,
+                type=mm_schemas.ModelEndpointMonitoringMetricType.METRIC,
+            )
+            metrics_values.append(
+                mm_schemas.ModelEndpointMonitoringMetricValues(
+                    full_name=full_name,
+                    values=list(
+                        zip(
+                            sub_df.index,
+                            sub_df[mm_schemas.MetricData.METRIC_VALUE],
+                        )
+                    ),  # pyright: ignore[reportArgumentType]
+                )
+            )
+            del metrics_without_data[full_name]
+
+        for metric in metrics_without_data.values():
+            metrics_values.append(
+                mm_schemas.ModelEndpointMonitoringMetricNoData(
+                    full_name=metric.full_name,
+                    type=mm_schemas.ModelEndpointMonitoringMetricType.METRIC,
+                )
+            )
+
+        return metrics_values
+
+    @staticmethod
+    def df_to_results_values(
+        *,
+        df: pd.DataFrame,
+        metrics: list[mm_schemas.ModelEndpointMonitoringMetric],
+        project: str,
+    ) -> list[
+        typing.Union[
+            mm_schemas.ModelEndpointMonitoringResultValues,
+            mm_schemas.ModelEndpointMonitoringMetricNoData,
+        ]
+    ]:
+        """
+        Parse a time-indexed data-frame of results from the TSDB into a list of
+        results values per distinct results.
+        When a result is not found in the data-frame, it is represented in no-data object.
+        """
+        metrics_without_data = {metric.full_name: metric for metric in metrics}
+
+        metrics_values: list[
+            typing.Union[
+                mm_schemas.ModelEndpointMonitoringResultValues,
+                mm_schemas.ModelEndpointMonitoringMetricNoData,
+            ]
+        ] = []
+        if not df.empty:
+            grouped = df.groupby(
+                [
+                    mm_schemas.WriterEvent.APPLICATION_NAME,
+                    mm_schemas.ResultData.RESULT_NAME,
+                ],
+                observed=False,
+            )
+        else:
+            grouped = []
+            logger.debug("No results", missing_results=metrics_without_data.keys())
+        for (app_name, name), sub_df in grouped:
+            result_kind = mlrun.model_monitoring.helpers.get_invocations_fqn(sub_df)
+            full_name = _compose_full_name(project=project, app=app_name, name=name)
+            metrics_values.append(
+                mm_schemas.ModelEndpointMonitoringResultValues(
+                    full_name=full_name,
+                    result_kind=result_kind,
+                    values=list(
+                        zip(
+                            sub_df.index,
+                            sub_df[mm_schemas.ResultData.RESULT_VALUE],
+                            sub_df[mm_schemas.ResultData.RESULT_STATUS],
+                        )
+                    ),  # pyright: ignore[reportArgumentType]
+                )
+            )
+            del metrics_without_data[full_name]
+
+        for metric in metrics_without_data.values():
+            if metric.full_name == mlrun.model_monitoring.helpers.get_invocations_fqn(project):
+                continue
+            metrics_values.append(
+                mm_schemas.ModelEndpointMonitoringMetricNoData(
+                    full_name=metric.full_name,
+                    type=mm_schemas.ModelEndpointMonitoringMetricType.RESULT,
+                )
+            )
+        print('[EYAL]: now return metrics mvalues: ', metrics_values)
+        return metrics_values
+
     def read_metrics_data(
         self,
         *,
@@ -264,9 +396,11 @@ class TDEngineConnector(TSDBConnector):
         if type == "metrics":
             table = mm_schemas.TDEngineSuperTables.METRICS
             name = mm_schemas.MetricData.METRIC_NAME
+            df_handler = self.df_to_metrics_values
         elif type == "results":
             table = mm_schemas.TDEngineSuperTables.APP_RESULTS
             name = mm_schemas.ResultData.RESULT_NAME
+            df_handler = self.df_to_results_values
         else:
             raise mlrun.errors.MLRunInvalidArgumentError(
                 f"Invalid type {type}, must be either 'metrics' or 'results'."
@@ -280,15 +414,45 @@ class TDEngineConnector(TSDBConnector):
             filter_query = query.getvalue()
 
 
-        # df = self.get_records(
-        #     table=table,
-        #     start=start,
-        #     end=end,
-        #     columns=[metric],
-        #     filter_query=filter_query,
-        #     timestamp_column=mm_schemas.WriterEvent.END_INFER_TIME
-        # )
-        #     full_name = mlrun.model_monitoring.helpers.get_metrics_fqn(self.project, app, metric)
+        df = self.get_records(
+            table=table,
+            start=start,
+            end=end,
+            # columns=[metric],
+            filter_query=filter_query,
+            timestamp_column=mm_schemas.WriterEvent.END_INFER_TIME
+        )
+
+        df[mm_schemas.WriterEvent.END_INFER_TIME] = pd.to_datetime(df[mm_schemas.WriterEvent.END_INFER_TIME])
+        df.set_index(mm_schemas.WriterEvent.END_INFER_TIME, inplace=True)
+
+        logger.debug(
+            "Read a data-frame",
+            project=self.project,
+            endpoint_id=endpoint_id,
+            is_empty=df.empty,
+        )
+
+        return df_handler(df=df, metrics=metrics, project=self.project)
+
+        # for metric in metrics:
+        #     full_name = _compose_full_name(project=self.project, app=metric.app, name=metric.name)
+        #     if df.empty:
+        #         results.append(
+        #             mm_schemas.ModelEndpointMonitoringMetricNoData(
+        #                 full_name=full_name,
+        #                 type=mm_schemas.ModelEndpointMonitoringMetricType.METRIC,
+        #             )
+        #         )
+        #     else:
+        #         results.append(
+        #             mm_schemas.ModelEndpointMonitoringMetricValues(
+        #                 full_name=full_name,
+        #                 values=list(zip(df._wend, df[metric])),
+        #             )
+        #         )
+        #
+        # full_name = _compose_full_name(project=self.project, endpoint_id=endpoint_id)
         #     if df.empty:
         #         results.append(
         #             mm_schemas.ModelEndpointMonitoringMetricNoData(
