f8b84b38acbb0719c6ff5e5419b977c940fa4a24
Eyal-Danieli <eyald@iguazio.com>
2024-06-02 16:40:31 +0300

    enrich model endpoint with tag and labels

--- tests/system/model_monitoring/test_model_monitoring.py
+++ tests/system/model_monitoring/test_model_monitoring.py
@@ -250,7 +250,8 @@ class TestBasicModelMonitoring(TestMLRunSystem):
     def test_basic_model_monitoring(self) -> None:
         # Main validations:
         # 1 - a single model endpoint is created
-        # 2 - stream metrics are recorded as expected under the model endpoint
+        # 2 - model labels and tag are recorded as expected under the model endpoint
+        # 3 - stream metrics are recorded as expected under the model endpoint
 
         # Deploy Model Servers
         project = self.project
@@ -278,6 +279,8 @@ class TestBasicModelMonitoring(TestMLRunSystem):
         )
 
         model_name = "sklearn_RandomForestClassifier"
+        tag = "some-tag"
+        labels = {"framework": "sklearn", "mylabel": "l1"}
 
         # Upload the model through the projects API so that it is available to the serving function
         project.log_model(
@@ -286,12 +289,14 @@ class TestBasicModelMonitoring(TestMLRunSystem):
             model_file="model.pkl",
             training_set=train_set,
             artifact_path=f"v3io:///projects/{project.metadata.name}",
+            tag=tag,
+            labels=labels,
         )
         # Add the model to the serving function's routing spec
         serving_fn.add_model(
             model_name,
             model_path=project.get_artifact_uri(
-                key=model_name, category="model", tag="latest"
+                key=model_name, category="model", tag=tag
             ),
         )
         if self.image is not None:
@@ -312,16 +317,20 @@ class TestBasicModelMonitoring(TestMLRunSystem):
 
         # Test metrics
         sleep(5)
-        self._assert_model_endpoint_metrics()
-
-    def _assert_model_endpoint_metrics(self) -> None:
         endpoints_list = mlrun.get_run_db().list_model_endpoints(
             self.project_name, metrics=["predictions_per_second"]
         )
         assert len(endpoints_list) == 1
 
         endpoint = endpoints_list[0]
+        self._assert_model_endpoint_tags_and_labels(
+            endpoint=endpoint, model_name=model_name, tag=tag, labels=labels
+        )
+        self._assert_model_endpoint_metrics(endpoint=endpoint)
 
+    def _assert_model_endpoint_metrics(
+        self, endpoint: mlrun.model_monitoring.model_endpoint.ModelEndpoint
+    ) -> None:
         assert len(endpoint.status.metrics) > 0
         self._logger.debug("Model endpoint metrics", endpoint.status.metrics)
 
@@ -332,6 +341,16 @@ class TestBasicModelMonitoring(TestMLRunSystem):
         ]
         total = sum(m[1] for m in predictions_per_second)
         assert total > 0
+
+    def _assert_model_endpoint_tags_and_labels(
+        self,
+        endpoint: mlrun.model_monitoring.model_endpoint.ModelEndpoint,
+        model_name: str,
+        tag: str,
+        labels: dict[str, str],
+    ) -> None:
+        assert endpoint.metadata.labels == labels
+        assert endpoint.spec.model == f"{model_name}:{tag}"
 
 
 @pytest.mark.skip(reason="Chronically fails, see ML-5820")
