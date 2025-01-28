d392cf3c9a81befd4e7e34db5959a095e0fe1897
Eyal-Danieli <eyald@iguazio.com>
2025-01-28 17:30:11 +0200

    fix test

--- tests/model_monitoring/db/tsdb/tdengine/test_tdengine_connector.py
+++ tests/model_monitoring/db/tsdb/tdengine/test_tdengine_connector.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import os
 import uuid
 from collections.abc import Iterator
 from datetime import datetime, timezone
